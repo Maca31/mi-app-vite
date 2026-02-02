02d50d30bc576d20abe48f050a18b50fbc218ca1
Maca31 <camilaveinte@gmail.com>
2026-02-02 10:14:23 +0100

    Proyecto Vite inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Despliegue Web CI/CD</h1>
+        <p>
+          Proyecto realizado con <strong>Vite + React</strong> y desplegado
+          automáticamente usando <strong>GitHub Actions</strong>.
+        </p>
+        <button>Ver repositorio</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
